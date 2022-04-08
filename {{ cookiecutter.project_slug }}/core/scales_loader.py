807b8d0649dd0496cc024223dbd94a5f11f5fd18
Daniel <daniel.jaek@gmail.com>
2022-04-08 12:40:14 +0200

    fix scales import

--- {{ cookiecutter.project_slug }}/core/scales_loader.py
+++ {{ cookiecutter.project_slug }}/core/scales_loader.py
@@ -7,9 +7,10 @@ def load_scales(bot: CustomSnake):
     """Automatically load all scales in the ./scales folder"""
 
     bot.logger.info("Loading Scales...")
+
     # go through all folders in the directory and load the scales from all files
     # Note: files must end in .py
-    for root, dirs, files in os.walk("./scales"):
+    for root, dirs, files in os.walk("scales"):
         for file in files:
             if file.endswith(".py") and not file.startswith("__init__"):
                 file = file.removesuffix(".py")
