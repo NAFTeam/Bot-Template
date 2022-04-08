32e6b09b4a8a59587a30c2c93e72ee791d03e146
Daniel <daniel.jaek@gmail.com>
2022-04-08 12:50:15 +0200

    add a component example

--- {{ cookiecutter.project_slug }}/scales/my_command.py
+++ {{ cookiecutter.project_slug }}/scales/my_command.py
@@ -1,6 +1,13 @@
 from core.base import CustomSnake
 
-from dis_snek import InteractionContext, Scale, slash_command
+from dis_snek import (
+    Button,
+    ButtonStyles,
+    ComponentContext, Embed,
+    InteractionContext,
+    Scale,
+    component_callback, slash_command,
+)
 
 
 class CommandScale(Scale):
@@ -10,7 +17,25 @@ class CommandScale(Scale):
     async def my_command(self, ctx: InteractionContext):
         """Says hello to the world"""
 
-        await ctx.send("Hello World")
+        # adds a component to the message
+        components = Button(
+            style=ButtonStyles.GREEN,
+            label="Hiya",
+            custom_id="hello_world_button"
+        )
+
+        # adds an embed to the message
+        embed = Embed(title="Hello World 2", description="Now extra fancy")
+
+        # respond to the interaction
+        await ctx.send("Hello World", embeds=embed, components=components)
+
+
+    @component_callback("hello_world_button")
+    async def my_callback(self, ctx: ComponentContext):
+        """Callback for the component from the hello_world command"""
+
+        await ctx.send("Hiya to you too")
 
 
 def setup(bot: CustomSnake):
